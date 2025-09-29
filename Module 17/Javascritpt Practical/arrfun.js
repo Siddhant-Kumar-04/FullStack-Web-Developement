145e99478d9d02007f233b8a9d9a1a17d3893940
Siddhant-Kumar-04 <Siddhant-Kumar-04@users.noreply.github.com>
2025-09-29 14:41:35 +0530

    Function with Unlimited Arguments

---
+++ Module 17/Javascritpt Practical/arrfun.js
@@ -0,0 +1,23 @@
+function arrAdd(arr)
+{
+    for (let i = 0; i < arr.length; i++)
+    {
+        arr[i] = arr[i] + 1;
+    }
+
+    return arr;
+}
+
+// Sum of all parameters;
+function arguAdd() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+    }
+    return sum;
+}
+
+console.log(arguAdd(1, 2));
+
+
+
