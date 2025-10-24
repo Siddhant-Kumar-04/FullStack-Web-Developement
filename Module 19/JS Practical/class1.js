4a8af8918a91fbf3add6803053a5cdaac168a0f3
Siddhant-Kumar-04 <Siddhant-Kumar-04@users.noreply.github.com>
2025-10-24 12:00:53 +0530

    Classes and Objects Started

---
+++ Module 19/JS Practical/class1.js
@@ -0,0 +1,11 @@
+class Products{
+    constructor() {
+        console.log("Products Constructor Called");
+    }
+    display() {
+        console.log("This is a Products");
+    }
+}
+
+const p= new Products();
+p.display();
