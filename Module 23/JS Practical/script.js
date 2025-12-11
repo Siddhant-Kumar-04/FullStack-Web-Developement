47d786950132cf64ec53e65e802eecb5d607f703
Siddhant-Kumar-04 <Siddhant-Kumar-04@users.noreply.github.com>
2025-12-11 14:12:48 +0530

    Creating of Tier List Using Javascript DOM Manipulation

--- Module 23/JS Practical/script.js
+++ Module 23/JS Practical/script.js
@@ -1,7 +1,25 @@
 
-
+const tier = document.getElementById('tier-input');
 const submitBtn1 = document.getElementById('clickMeButton');
 submitBtn1.addEventListener('click', (event)=> {
-    console.log('Button Clicked');
-    event.preventDefault(); //stops the default behaviour of form submission
+    alert('Button Clicked');
+    event.preventDefault()
+    // const trg = event.target;
+    // alert(trg);
+    createTierList(tier.value);
+    tier.value = '';
 })
+
+function createTierList(tierlistName) {
+    const newTierList = document.createElement('div');
+    newTierList.classList.add('tier-list');
+    const heading = document.createElement('h1');
+    heading.textContent = tierlistName;
+    const newTierListItem = document.createElement('div');
+    newTierListItem.classList.add('tier-list-item');
+    newTierList.appendChild(heading);
+    newTierList.appendChild(newTierListItem);
+
+    const tierSection = document.getElementById('tier-list-section');
+    tierSection.appendChild(newTierList);
+}
